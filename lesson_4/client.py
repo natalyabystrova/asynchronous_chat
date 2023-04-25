60948dd8d000792bb0f50c050ef19674a07c3647
natalyabystrova <98619051+natalyabystrova@users.noreply.github.com>
2023-04-25 13:44:17 +0300

    lesson_6 (#6)

--- lesson_4/client.py
+++ lesson_4/client.py
@@ -5,15 +5,16 @@ import time
 import argparse
 import logging
 from logs import client_log_config
-from errors import ReqFieldMissingError
 from constantsnutils import constants
 from constantsnutils import utils
+from errors import ReqFieldMissingError
+from decos import log
 
 
+LOGGER = logging.getLogger('client')
 
-CLIENT_LOGGER = logging.getLogger('client')
 
-
+@log
 def create_presence(account_name='Guest'):
     out = {
         constants.ACTION: constants.PRESENCE,
@@ -22,12 +23,13 @@ def create_presence(account_name='Guest'):
             constants.ACCOUNT_NAME: account_name
         }
     }
-    CLIENT_LOGGER.debug(f'Сформировано {constants.PRESENCE} сообщение для пользователя {account_name}')
+    LOGGER.debug(f'Сформировано {constants.PRESENCE} сообщение для пользователя {account_name}')
     return out
 
 
+@log
 def process_ans(message):
-    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
+    LOGGER.debug(f'Разбор сообщения от сервера: {message}')
     if constants.RESPONSE in message:
         if message[constants.RESPONSE] == 200:
             return '200 : OK'
@@ -35,6 +37,7 @@ def process_ans(message):
     raise ReqFieldMissingError(constants.RESPONSE)
 
 
+@log
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('addr', default=constants.DEFAULT_IP_ADDRESS, nargs='?')
@@ -48,14 +51,16 @@ def main():
     server_address = namespace.addr
     server_port = namespace.port
 
+    # проверим подходящий номер порта
     if not 1023 < server_port < 65536:
-        CLIENT_LOGGER.critical(
-            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
-            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
+        LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}. '
+            f'Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
-    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
-                       f'адрес сервера: {server_address}, порт: {server_port}')
+    LOGGER.info(f'Запущен клиент с парамертами: адрес сервера: '
+                f'{server_address}, порт: {server_port}')
+    # Инициализация сокета и обмен
 
     try:
         transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,16 +68,14 @@ def main():
         message_to_server = create_presence()
         utils.send_message(transport, message_to_server)
         answer = process_ans(utils.make_message(transport))
-        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
-        print(answer)
+        LOGGER.info(f'Принят ответ от сервера {answer}')
     except json.JSONDecodeError:
-        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
+        LOGGER.error('Не удалось декодировать полученную Json строку.')
     except ReqFieldMissingError as missing_error:
-        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
-                            f'{missing_error.missing_field}')
+        LOGGER.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
     except ConnectionRefusedError:
-        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
-                               f'конечный компьютер отверг запрос на подключение.')
+        LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                        f'конечный компьютер отверг запрос на подключение.')
 
 
 if __name__ == '__main__':
--- lesson_4/server.py
+++ lesson_4/server.py
@@ -6,11 +6,16 @@ import logging
 from errors import IncorrectDataRecivedError
 from constantsnutils import constants
 from constantsnutils import utils
+import logs.server_log_config
+from decos import log
 
-SERVER_LOGGER = logging.getLogger('server')
 
+LOGGER = logging.getLogger('server')
+
+
+@log
 def process_client_message(message):
-    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    LOGGER.debug(f'Разбор сообщения от клиента : {message}')
     if constants.ACTION in message and message[constants.ACTION] == constants.PRESENCE and constants.TIME in message and \
             constants.USER in message and message[constants.USER][constants.ACCOUNT_NAME] == 'Guest':
         return {constants.RESPONSE: 200}
@@ -20,7 +25,9 @@ def process_client_message(message):
     }
 
 
+@log
 def create_arg_parser():
+    """Парсер аргументов коммандной строки"""
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=constants.DEFAULT_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
@@ -28,43 +35,47 @@ def create_arg_parser():
 
 
 def main():
+    """Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию"""
     parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
 
     if not 1023 < listen_port < 65536:
-        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
-                               f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта {listen_port}. '
+                        f'Допустимы адреса с 1024 до 65535.')
         sys.exit(1)
-    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
-                       f'адрес с которого принимаются подключения: {listen_address}. '
-                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+    LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, адрес,'
+                f' с которого принимаются подключения: {listen_address}. '
+                f'Если адрес не указан, принимаются соединения с любых адресов.')
+    # Готовим сокет
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
 
+    # Слушаем порт
 
     transport.listen(constants.MAX_CONNECTIONS)
 
     while True:
         client, client_address = transport.accept()
-        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+        LOGGER.info(f'Установлено соедение с ПК {client_address}')
         try:
             message_from_cient = utils.make_message(client)
-            SERVER_LOGGER.debug(f'Получено сообщение {message_from_cient}')
+            LOGGER.debug(f'Получено сообщение {message_from_cient}')
+            print(message_from_cient)
             response = process_client_message(message_from_cient)
-            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
+            LOGGER.info(f'Cформирован ответ клиенту {response}')
             utils.send_message(client, response)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
             client.close()
         except json.JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
-                                f'клиента {client_address}. Соединение закрывается.')
+            LOGGER.error(f'Не удалось декодировать Json строку, '
+                         f'полученную от клиента {client_address}. Соединение закрывается.')
             client.close()
         except IncorrectDataRecivedError:
-            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
-                                f'Соединение закрывается.')
+            LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
+                         f'Соединение закрывается.')
             client.close()
 
 
