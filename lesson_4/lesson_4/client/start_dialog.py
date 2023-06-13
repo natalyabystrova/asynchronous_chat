d914717301e18fef303378d3ac6dd225e9e0f700
Tash <natalya.bystrova.93@mail.ru>
2023-06-13 13:53:29 +0300

    lesson_last

---
+++ lesson_4/lesson_4/client/start_dialog.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import QDialog, QPushButton, QLineEdit, QApplication, QLabel , qApp
+from PyQt5.QtCore import QEvent
+
+
+# Стартовый диалог с выбором имени пользователя
+class UserNameDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.ok_pressed = False
+
+        self.setWindowTitle('Привет!')
+        self.setFixedSize(200, 150)
+
+        self.label = QLabel('Введите имя пользователя:', self)
+        self.label.move(10, 10)
+        self.label.setFixedSize(200, 10)
+
+        self.client_name = QLineEdit(self)
+        self.client_name.setFixedSize(154, 20)
+        self.client_name.move(10, 30)
+
+        self.label_psw = QLabel('Введите пароль:', self)
+        self.label_psw.move(10, 60)
+        self.label_psw.setFixedSize(275, 10)
+
+        self.client_passwd = QLineEdit(self)
+        self.client_passwd.setFixedSize(154, 20)
+        self.client_passwd.move(10, 75)
+        self.client_passwd.setEchoMode(QLineEdit.Password)
+
+        self.btn_ok = QPushButton('Начать', self)
+        self.btn_ok.move(20, 105)
+        self.btn_ok.clicked.connect(self.click)
+
+        self.btn_cancel = QPushButton('Выход', self)
+        self.btn_cancel.move(100, 105)
+        self.btn_cancel.clicked.connect(qApp.exit)
+
+        self.show()
+
+    # Обработчик кнопки ОК, если поле вводе не пустое, ставим флаг и завершаем приложение.
+    def click(self):
+        if self.client_name.text():
+            self.ok_pressed = True
+            qApp.exit()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    dial = UserNameDialog()
+    app.exec_()
